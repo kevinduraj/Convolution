f8be6eb71bfe437fbc45e0b3e1638beff0a2c9f6
Kevin Duraj <kevinduraj@gmail.com>
2014-03-21 19:30:32 -0700

    convolution

---
+++ Gaussian2D/src/gaussian2d/Gaussian2D.java
@@ -0,0 +1,12 @@
+package gaussian2d;
+
+public class Gaussian2D {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
